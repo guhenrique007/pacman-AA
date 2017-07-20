ce765a82c78b97629fba095615bf59abf7c99eeb
Renato Ferreira <renatoferreira249@gmail.com>
2017-07-20 13:01:57 -0300

    soh falta os comentarios e o readme

--- pacmanAgents.py
+++ pacmanAgents.py
@@ -26,6 +26,41 @@ class LeftTurnAgent(game.Agent):
     if Directions.LEFT[left] in legal: return Directions.LEFT[left]
     return Directions.STOP
 
+class MeuAgente(Agent):     
+  def getPacmanSuccessors(self, legalActions, state):
+    return [(state.generateSuccessor(self.index, action), action, state.generateSuccessor(self.index, action).getPacmanPosition()) for action in legalActions]
+
+  def getAction(self, state):
+    from util import PriorityQueue, Counter, manhattanDistance
+    
+    gridComidas = state.getFood()
+    posicaoComidas = list(gridComidas.asList())
+    _, posProxComida = min([(manhattanDistance(state.getPacmanPosition(), pos)
+        ,pos) for pos in posicaoComidas])
+    posVisitadas = set()
+
+    heap = PriorityQueue()
+    for proxEstado, proxAcao, proxPos in self.getPacmanSuccessors(
+        state.getLegalActions(self.index),state):
+      prioridade =  manhattanDistance(proxPos, posProxComida) + 1
+      heap.push((proxEstado, proxAcao, 1), prioridade)
+      posVisitadas.add(proxPos)
+
+    while not heap.isEmpty():
+      estadoAtual, primAcao, numAcoes = heap.pop()
+      if estadoAtual.getPacmanPosition() == posProxComida:
+        return primAcao
+      for proxEstado, proxAcao, proxPos in self.getPacmanSuccessors(
+          estadoAtual.getLegalActions(self.index),estadoAtual):
+          if not proxEstado.getPacmanPosition() in estadoAtual.getGhostPositions():
+            if proxEstado.getPacmanPosition() not in posVisitadas:
+              numAcoes += 1
+              heuristica = manhattanDistance(proxPos, posProxComida)
+              heap.push((proxEstado, primAcao, numAcoes), numAcoes + heuristica)
+              posVisitadas.add(proxPos)
+    print "sai"
+    return random.choice(state.getLegalPacmanActions())
+
 class GreedyAgent(Agent):
   def __init__(self, evalFn="scoreEvaluation"):
     self.evaluationFunction = util.lookup(evalFn, globals())
