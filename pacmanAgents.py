c3b2c9d6d352e87a37e2bfa1417d4d8812402adc
Renato Ferreira <renatoferreira249@gmail.com>
2017-07-20 22:11:27 -0300

    comentado

--- pacmanAgents.py
+++ pacmanAgents.py
@@ -26,7 +26,6 @@ class LeftTurnAgent(game.Agent):
     if Directions.LEFT[left] in legal: return Directions.LEFT[left]
     return Directions.STOP
 
-<<<<<<< HEAD
 class MeuAgente(Agent):     
   def getPacmanSuccessors(self, legalActions, state):
     return [(state.generateSuccessor(self.index, action), action, state.generateSuccessor(self.index, action).getPacmanPosition()) for action in legalActions]
@@ -34,48 +33,46 @@ class MeuAgente(Agent):
   def getAction(self, state):
     from util import PriorityQueue, Counter, manhattanDistance
     
-	#extrai posicao das comidas para que o pacman possa se orientar
+    #extrai posicao das comidas para que o pacman possa se orientar
     gridComidas = state.getFood()
-	#transforma em lista para ser utilizada em iteracoes posteriores
+    #transforma em lista para ser utilizada em iteracoes posteriores
     posicaoComidas = list(gridComidas.asList())
-	#posição da primeira comida que sera perseguida pelo pacman
+    #posicao da primeira comida que sera perseguida pelo pacman
     _, posProxComida = min([(manhattanDistance(state.getPacmanPosition(), pos)
         ,pos) for pos in posicaoComidas])
-    #armazena posições ja percorridas, para que o a* não fique em loop infinito
-	posVisitadas = set()
+    #armazena posicoes ja percorridas, para que o a* nao fique em loop infinito
+    posVisitadas = set()
 
     heap = PriorityQueue()
 	#armazena estados sucessores ao estado inicial no heap de caminhos
     for proxEstado, proxAcao, proxPos in self.getPacmanSuccessors(
-        state.getLegalActions(self.index),state):
-	  # heuristica eh a distancia de manhattan e o custo de um estado para um estado
-	  # imediatamente adjacente
+      state.getLegalActions(self.index),state):
+      # heuristica eh a distancia de manhattan e o custo de um estado para um estado
+      # imediatamente adjacente
       prioridade =  manhattanDistance(proxPos, posProxComida) + 1
       #coloca estados imediatamente adjacentes ao inicial no heap de caminhos
-	  heap.push((proxEstado, proxAcao, 1), prioridade)
+      heap.push((proxEstado, proxAcao, 1), prioridade)
       posVisitadas.add(proxPos)
 
     while not heap.isEmpty():
       estadoAtual, primAcao, numAcoes = heap.pop()
       if estadoAtual.getPacmanPosition() == posProxComida:
         #retorna primeira acao do caminho, pois o nelhor caminho foi achado
-		return primAcao
+	return primAcao
       for proxEstado, proxAcao, proxPos in self.getPacmanSuccessors(
           estadoAtual.getLegalActions(self.index),estadoAtual):
-          if not proxEstado.getPacmanPosition() in estadoAtual.getGhostPositions():
-            if proxEstado.getPacmanPosition() not in posVisitadas:
-              numAcoes += 1
-              heuristica = manhattanDistance(proxPos, posProxComida)
-              #coloca novo estado no heap para continuar montando caminho
-			  heap.push((proxEstado, primAcao, numAcoes), numAcoes + heuristica)
-              #marca posicao como visitada, para que o pacman não fique em loop infinito
-			  posVisitadas.add(proxPos)
+        if not proxEstado.getPacmanPosition() in estadoAtual.getGhostPositions():
+          if proxEstado.getPacmanPosition() not in posVisitadas:
+            numAcoes += 1
+            heuristica = manhattanDistance(proxPos, posProxComida)
+            #coloca novo estado no heap para continuar montando caminho
+	    heap.push((proxEstado, primAcao, numAcoes), numAcoes + heuristica)
+            #marca posicao como visitada, para que o pacman nao fique em loop infinito
+	    posVisitadas.add(proxPos)
     
-	# retorna acao aleatoria, pois nesse ponto não ha mais acao boa
+    # retorna acao aleatoria, pois nesse ponto nao ha mais acao boa
     return random.choice(state.getLegalPacmanActions())
 
-=======
->>>>>>> parent of ce765a8... soh falta os comentarios e o readme
 class GreedyAgent(Agent):
   def __init__(self, evalFn="scoreEvaluation"):
     self.evaluationFunction = util.lookup(evalFn, globals())
